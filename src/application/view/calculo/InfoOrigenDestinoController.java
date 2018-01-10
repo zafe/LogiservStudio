cd5dd661a08cfb4aeec485a649fe6815a9a73332
isaias <isamed92@gmail.com>
2018-01-10 18:02:25 -0300

    Merge branch 'master' of https://github.com/zafe/LogiservStudio
    # Conflicts:
    #	src/application/repository/info/ArticuloRepository.java
    #	src/application/repository/info/CategoriaArticuloReporsitory.java
    #	src/application/repository/info/DetalleCompraRepository.java
    #	src/application/repository/info/FacturaCompraRepository.java
    #	src/application/repository/info/ProveedorRepository.java

---
+++ src/application/view/calculo/InfoOrigenDestinoController.java
@@ -0,0 +1,40 @@
+package application.view.calculo;
+
+import application.model.calculo.Ingenio;
+import application.model.calculo.OrigenDestino;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+
+import javax.swing.text.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InfoOrigenDestinoController implements Initializable {
+    @FXML
+    private TableView distanciaTableView;
+    @FXML
+    private TableColumn<Ingenio, String> idColumn;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
