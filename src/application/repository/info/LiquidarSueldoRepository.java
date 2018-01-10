4779eb207ec11f6fbe6f66aec978ec795cf5f2d3
Fernando Zafe <fernandozafe@gmail.com>
2018-01-10 17:59:58 -0300

    Edicion de LiquidarSueldoRepository

--- src/application/repository/info/LiquidarSueldoRepository.java
+++ src/application/repository/info/LiquidarSueldoRepository.java
@@ -1,9 +1,11 @@
 package application.repository.info;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 
 import application.database.JDBCConnection;
 import application.model.info.Empleado;
@@ -22,7 +24,18 @@ public class LiquidarSueldoRepository {
         try {
 			preparedStatement=connection.prepareStatement("INSERT INTO LIQUIDACION_EMPLEADO " +
 			        "VALUES(?,?,?,?,?,?,?,?,?)");
-	        preparedStatement.setString(1,null);
+
+			java.util.Date fecha_liquidacion = new java.util.Date();
+			preparedStatement.setDate(1, (java.sql.Date ) fecha_liquidacion );//fecha_liquidacion
+	        preparedStatement.setDouble(2,0.0);//importe_neto
+	        preparedStatement.setDouble(3,0.0);//total_haberes_remunerativos
+	        preparedStatement.setDouble(4,0.0);//total_haberes_no_remunerativos
+	        preparedStatement.setDouble(5,0.0);//total_retenciones
+	        preparedStatement.setDouble(6,0.0);//total_bruto
+	        preparedStatement.setInt(7,empleado.getIdEmpleado());//EMPLEADO_idEmpleado
+	        preparedStatement.setDate(8,null);//inicio_periodo
+	        preparedStatement.setDate(9,null);//fin_periodo
+	        preparedStatement.execute();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
